35b83fba0dccebe459d8d7ae1f18c2f984588d4d
Dejun Jackson <dejunjackson16@gmail.com>
2021-12-03 07:25:51 -0600

    initial commit

---
+++ author.py
@@ -0,0 +1,9 @@
+class Author:
+    def __init__(self):
+        self.name = "Dejun Jackson"
+        self.email = "dejunjackson16@gmail.com"
+        self.website = "dejunjackson.com"
+        self.github = "https://github.com/DejunJackson"
+        self.linkedIn = "https://www.linkedin.com/in/dejun-jackson/"
+        self.twitter = "https://twitter.com/Afro_codes"
+        self.hobbies = ['Video Games', 'Watching HBO Max with my wife', 'Coding cool projects with cool people', 'Watching my dogs wrestle in the snow']
