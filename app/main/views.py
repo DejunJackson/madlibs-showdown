526bb2e7d38d2e7e1140cfd37449de41caa97cef
Dejun Jackson <dejunjackson16@gmail.com>
2021-12-15 19:21:58 -0600

    add logic for leaderboards

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import RegisterForm
 from django.urls import reverse
+from .models import User
 # Create your views here.
 
 def index(request):
@@ -23,3 +24,9 @@ def register(request):
         form = RegisterForm()
 
     return render(request, "registration/register.html", {"form":form})
+
+def leaderboards(request):
+    all_players = User.objects.order_by('-score')
+    user = request.user
+    context = {'players': all_players, 'user': user}
+    return render(request, 'main/leaderboards.html', context)
